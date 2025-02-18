b679ea0a4138c4f320c0b80231c76510ced26c15
Ali Magdy <166527591+alimagdye@users.noreply.github.com>
2025-02-18 03:56:11 +0200

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -4,4 +4,4 @@
 import * as dotenv from "dotenv";
 dotenv.config(); // load environment variables from a .env file into process.env
 
-import "./server"; // import the server file to start the server
+import "./server.js"; // import the server file to start the server
