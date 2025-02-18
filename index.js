a5cf7032353b728762a46e49879dd62c2a65e665
Ali Magdy <166527591+alimagdye@users.noreply.github.com>
2025-02-18 03:55:39 +0200

    Update notes

--- index.js
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env node
-"use strict";
-
-import * as dotenv from "dotenv";
-dotenv.config(); // load environment variables from a .env file into process.env
-
-import "./server.js"; // import the server file to start the server
